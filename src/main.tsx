52cbb0af49d7653b855e42de4778ead82f6ff87d
lehoangvuvt <hoangvule100@gmail.com>
2025-04-09 22:43:18 +0700

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
 const router = createRouter({ routeTree })
-
+asdaddasd
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
   interface Register {
