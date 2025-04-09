2b88487abb2b42fdd3098725be94597de65e48f8
lehoangvuvt <hoangvule100@gmail.com>
2025-04-09 22:54:23 +0700

    re init commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
-    react(),
-  ],
-});
+  plugins: [react()],
+})
